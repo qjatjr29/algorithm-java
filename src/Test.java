fbb5edfb24824e7e6f3cd5411833f5f6635ee1cc
KoBeomSeok <qjatjr29@khu.ac.kr>
2022-01-27 11:29:52 +0900

    Init Commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("입력 후 enter: ");
+        int n =sc.nextInt();
+        System.out.println("n : "+n);
+    }
+}
