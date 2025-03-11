a2befdfd279b21da53cb49136fdee4a7d8cb990f
beomsic <hhqjatjr@gmail.com>
2025-03-11 10:24:51 +0900

    :sparkles: 구슬탈출2

--- src/baekJoon/Implementation/구슬탈출2.java
+++ src/baekJoon/Implementation/구슬탈출2.java
@@ -1,227 +1,156 @@
 package baekJoon.Implementation;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.*;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class 구슬탈출2 {
 
-    private static final int WALL = 1;
-    private static final int EMPTY = 0;
-    private static final int BALL = 2;
-    private static final int HOLE = 3;
+    private static final char HALL = 'O';
+    private static final char WALL = '#';
+    private static int tx, ty;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
+    private static boolean[][][][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer input = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(input.nextToken());
-        int M = Integer.parseInt(input.nextToken());
+        int n = Integer.parseInt(input.nextToken());
+        int m = Integer.parseInt(input.nextToken());
 
         int answer = -1;
-        int[][] board = new int[N][M];
+        char[][] board = new char[n][m];
+        visited = new boolean[n][m][n][m];
 
-        Ball red = new Ball(0, 0);
-        Ball blue = new Ball(0, 0);
-
-        for(int i = 0; i < N; i++) {
+        int rx = 0, ry = 0;
+        int bx = 0, by = 0;
+        for (int i = 0; i < n; i++) {
             input = new StringTokenizer(br.readLine());
-            String inputStr = input.nextToken();
-            for(int j = 0; j < M; j++) {
-
-                char state = inputStr.charAt(j);
-                if(state == '#') {
-                    board[i][j] = WALL;
+            char[] chars = input.nextToken().toCharArray();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = chars[j];
+                if (board[i][j] == HALL) {
+                    tx = i;
+                    ty = j;
                 }
-
-                if(state == 'R') {
-                    red = new Ball(i, j);
+                if (board[i][j] == 'R') {
+                    rx = i;
+                    ry = j;
                 }
-
-                if(state == 'B') {
-                    blue = new Ball(i, j);
-                }
-
-                if(state == 'O') {
-                    board[i][j] = HOLE;
+                if (board[i][j] == 'B') {
+                    bx = i;
+                    by = j;
                 }
             }
         }
 
-        answer = moveBall(red, blue, board, answer);
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        pq.add(new Node(rx, ry, bx, by, 0));
+        visited[rx][ry][bx][by] = true;
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            int redX = cur.rx;
+            int redY = cur.ry;
+            int blueX = cur.bx;
+            int blueY = cur.by;
+
+            if (cur.count > 10) {
+                break;
+            }
+
+            if (redX == tx && redY == ty) {
+                answer = cur.count;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+
+                int[] nRed = tilt(redX, redY, i, board);
+                int[] nBlue = tilt(blueX, blueY, i, board);
+
+                // 파란 구슬이 구멍에 빠지는 경우는 안된다.
+                if (nBlue[0] == tx && nBlue[1] == ty) continue;
+
+                // 같은 장소로 이동했다면, 이동하는 방향에 더 가까웠던 구슬이 해당 장소에 위치.
+                if (nRed[0] == nBlue[0] && nRed[1] == nBlue[1]) {
+                    // 위
+                    if (i == 0) {
+                        if (redX < blueX) nBlue[0] += 1;
+                        else nRed[0] += 1;
+                    }
+                    // 아래
+                    if (i == 1) {
+                        if (redX > blueX) nBlue[0] -= 1;
+                        else nRed[0] -= 1;
+                    }
+                    // 왼쪽
+                    if (i == 2) {
+                        if (redY < blueY) nBlue[1] += 1;
+                        else nRed[1] += 1;
+                    }
+                    // 오른쪽
+                    if (i == 3) {
+                        if (redY > blueY) nBlue[1] -= 1;
+                        else nRed[1] -= 1;
+                    }
+                }
+
+                if (visited[nRed[0]][nRed[1]][nBlue[0]][nBlue[1]]) continue;
+                visited[nRed[0]][nRed[1]][nBlue[0]][nBlue[1]] = true;
+
+                pq.add(new Node(nRed[0], nRed[1], nBlue[0], nBlue[1], cur.count + 1));
+            }
+
+        }
 
         bw.write(String.valueOf(answer));
+        bw.newLine();
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static int moveBall(Ball red, Ball blue, int[][] board, int answer) {
+    private static int[] tilt(int x, int y, int dir, char[][] board) {
 
-        Direction[] directions = new Direction[] {Direction.LEFT,
-                Direction.RIGHT,
-                Direction.UP,
-                Direction.DOWN};
-
-        Queue<Node> pq = new ArrayDeque<>();
-        boolean[][][][] visited = new boolean[10][10][10][10];
-
-        visited[red.x][red.y][blue.x][blue.y] = true;
-        pq.add(new Node(red, blue, 0));
-
-        while(!pq.isEmpty()) {
-
-            Node here = pq.poll();
-            Ball redBall = here.redBall;
-            Ball blueBall = here.blueBall;
-            int count = here.count;
-
-            if(count > 10) {
-                continue;
-            }
-
-            if(redBall.isInHole(board)) {
-                answer = count;
+        while (true) {
+            x = x + dx[dir];
+            y = y + dy[dir];
+            if (board[x][y] == WALL) {
+                x -= dx[dir];
+                y -= dy[dir];
                 break;
             }
-
-           // 4가지 방향으로 이동
-            for(int i = 0; i < 4; i++) {
-                Direction direction = directions[i];
-                Ball nextRed = redBall.move(direction, board);
-                Ball nextBlue = blueBall.move(direction, board);
-
-                // 실패 케이스 - 파란색 공이 hole에 들어간 경우
-                if(nextBlue.isInHole(board)) {
-                    continue;
-                }
-
-                // 두 구슬이 같은 위치에 있는 경우 - 위치 수정해 주어야함
-                if(nextRed.isSamePosition(nextBlue)) {
-                    // 기울이기전에 기울이는 방향으로 더 많이 가있던 공은 그대로
-                    if(direction == Direction.LEFT) {
-                        if(redBall.y > blueBall.y) {
-                            nextRed.y++;
-                        }
-                        else nextBlue.y++;
-                    }
-
-                    if(direction == Direction.RIGHT) {
-                        if(redBall.y < blueBall.y) {
-                            nextRed.y--;
-                        }
-                        else nextBlue.y--;
-                    }
-
-                    if(direction == Direction.UP) {
-                        if(redBall.x > blueBall.x) {
-                            nextRed.x++;
-                        }
-                        else nextBlue.x++;
-                    }
-
-                    if(direction == Direction.DOWN) {
-                        if(redBall.x < blueBall.x) {
-                            nextRed.x--;
-                        }
-                        else nextBlue.x--;
-                    }
-                }
-
-                if(visited[nextRed.x][nextRed.y][nextBlue.x][nextBlue.y]) {
-                    continue;
-                }
-
-                visited[nextRed.x][nextRed.y][nextBlue.x][nextBlue.y] = true;
-                pq.add(new Node(nextRed, nextBlue, count + 1));
-            }
+            if (board[x][y] == HALL) break;
         }
-        return answer;
+        return new int[]{x, y};
     }
 
-    private static class Node {
-        Ball redBall;
-        Ball blueBall;
+    private static class Node implements Comparable<Node> {
+        int rx;
+        int ry;
+        int bx;
+        int by;
         int count;
 
-        public Node(Ball redBall, Ball blueBall, int count) {
-            this.redBall = redBall;
-            this.blueBall = blueBall;
+        public Node() { }
+
+        public Node(int rx, int ry, int bx, int by, int count) {
+            this.rx = rx;
+            this.ry = ry;
+            this.bx = bx;
+            this.by = by;
             this.count = count;
         }
-    }
 
-    private static class Ball {
-        int x;
-        int y;
-
-        public Ball(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Ball move(Direction dir, int[][] board) {
-
-            int[] d = dir.getDirection();
-            int cx = x;
-            int cy = y;
-
-            while(true) {
-
-                int nx = cx + d[0];
-                int ny = cy + d[1];
-
-                if(nx == 0 || ny == 0 || nx == board.length - 1 || ny == board[0].length - 1) {
-                    break;
-                }
-
-                if(board[nx][ny] == WALL) {
-                    break;
-                }
-
-                if(board[nx][ny] == HOLE) {
-                    cx = nx;
-                    cy = ny;
-                    break;
-                }
-
-                cx = nx;
-                cy = ny;
-            }
-
-            return new Ball(cx, cy);
-        }
-
-        public boolean isSamePosition(Ball other) {
-            return this.x == other.x && this.y == other.y;
-        }
-
-        public boolean isInHole(int[][] board) {
-            return board[x][y] == HOLE;
+        @Override
+        public int compareTo(Node o) {
+            return this.count - o.count;
         }
     }
 
-    public enum Direction {
-        UP(new int[] {-1, 0}),
-        DOWN(new int[] {1, 0}),
-        LEFT(new int[] {0, -1}),
-        RIGHT(new int[] {0, 1});
-
-        private int[] direction;
-
-        Direction(int[] direction) {
-            this.direction = direction;
-        }
-
-        public int[] getDirection() {
-            return direction;
-        }
-    }
 }
