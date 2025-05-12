72c05d3e6f95d32b6cac6a9eb875726769ac6784
beomsic <hhqjatjr@gmail.com>
2025-05-12 15:06:29 +0900

    :sparkles: 빵집

--- src/baekJoon/graph/dfs/빵집.java
+++ src/baekJoon/graph/dfs/빵집.java
@@ -1,70 +1,65 @@
 package baekJoon.graph.dfs;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class 빵집 {
+    private static final int[] dx = {-1, 0, 1};
+    private static final int[] dy = {1, 1, 1};
+    private static int answer;
+    private static boolean[][] visited;
 
-  private static int R, C, answer;
-  private static char[][] map;
-  private static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer input = new StringTokenizer(br.readLine());
 
-  public static void main(String[] args) throws IOException {
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int r = Integer.parseInt(input.nextToken());
+        int c = Integer.parseInt(input.nextToken());
+        char[][] map = new char[r][c];
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < r; i++) {
+            input = new StringTokenizer(br.readLine());
+            String s = input.nextToken();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
 
-    R = Integer.parseInt(st.nextToken());
-    C = Integer.parseInt(st.nextToken());
-    map = new char[R + 1][C + 1];
-    visited = new boolean[R + 1][C + 1];
+        visited = new boolean[r][c];
+        answer = 0;
+        for (int i = 0; i < r; i++) {
+            if (map[i][0] == 'x') continue;
+            dfs(map, i, 0);
+        }
 
-    for(int i = 1; i <= R; i++) {
-      st = new StringTokenizer(br.readLine());
-      String col = st.nextToken();
-      for(int j = 1; j <= C; j++) {
-        map[i][j] = col.charAt(j - 1);
-      }
+        bw.write(String.valueOf(answer));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    for(int i = 1; i <= R; i++) {
-        dfs(i, 1);
-    }
-    bw.write(String.valueOf(answer));
-    bw.newLine();
-    bw.flush();
-    bw.close();
-    br.close();
-  }
-  private static boolean dfs(int x, int y) {
-    // 기저
-//    if(x <= 0 || x > R || y > C) return false;
-//    if(map[x][y] == 'x') return false;
-//    if(visited[x][y]) return false;
-    if(y == C) {
-      answer++;
-      visited[x][y] = true;
-      return true;
-    }
+    private static boolean dfs(char[][] map, int x, int y) {
 
-    for(int i = 0; i < 3; i++) {
-      int nx = x + i - 1;
-      int ny = y + 1;
-      if(nx <= 0 || nx > R || ny > C) continue;
-      if(visited[nx][ny]) continue;
-      if(map[nx][ny] == 'x') continue;
-      if(dfs(x + i - 1, y + 1)) {
+        // 도착한 경우
+        if (y == map[0].length - 1) {
+            answer++;
+            visited[x][y] = true;
+            return true;
+        }
+        // 해당 칸 사용
         visited[x][y] = true;
-        return true;
-      }
-    }
-    visited[x][y] = true;
-    return false;
-  }
+        for (int i = 0; i < 3; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
+            if (nx < 0 || nx >= map.length || ny >= map[0].length || visited[nx][ny]) continue;
+            if (map[nx][ny] == 'x') continue;
+            // 하나의 경우라도 가스관 연결에 성공했다면 true 반환하고 해당 칸은 사용x
+            if (dfs(map, nx, ny)) return true;
+        }
+        // 가스관 연결에 실패 -> false 반환
+        return false;
+    }
 }
