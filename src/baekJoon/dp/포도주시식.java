9421efab7e22ec2dd04ef525f1e1675a2cb21c1d
beomsic <hhqjatjr@gmail.com>
2025-10-05 13:58:20 +0900

    :sparkles: 포도주시식

--- src/baekJoon/dp/포도주시식.java
+++ src/baekJoon/dp/포도주시식.java
@@ -1,10 +1,6 @@
 package baekJoon.dp;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class 포도주시식 {
@@ -16,35 +12,29 @@ public class 포도주시식 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer input = new StringTokenizer(br.readLine());
 
-        answer = 0;
-        int drinkCount = Integer.parseInt(input.nextToken());
+        int n = Integer.parseInt(input.nextToken());
 
-        int[] drinks = new int[drinkCount + 1];
-        int[][] dp = new int[drinkCount + 1][3];
+        int[] juice = new int[n];
 
-        for(int i = 1; i <= drinkCount; i++) {
+        for (int i = 0; i < n; i++) {
             input = new StringTokenizer(br.readLine());
-            drinks[i] = Integer.parseInt(input.nextToken());
+            juice[i] = Integer.parseInt(input.nextToken());
         }
 
-        for(int i = 1; i <= drinkCount; i++) {
-            // 포도주 마시는 경우
-            dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] + drinks[i]);
-            dp[i][2] = Math.max(dp[i][2], dp[i - 1][1] + drinks[i]);
+        int[][] dp = new int[n][3];
 
-            // 마시지 않는 경우
-            dp[i][0] = Math.max(dp[i][0], dp[i - 1][0]);
-            dp[i][0] = Math.max(dp[i][0], dp[i - 1][1]);
-            dp[i][0] = Math.max(dp[i][0], dp[i - 1][2]);
+        dp[0][1] = juice[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i][0], Math.max(dp[i - 1][2], Math.max(dp[i - 1][0], dp[i - 1][1])));
+            dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] + juice[i]);
+            dp[i][2] = Math.max(dp[i][2], dp[i - 1][1] + juice[i]);
         }
 
-        answer = Math.max(dp[drinkCount][0], dp[drinkCount][1]);
-        answer = Math.max(answer, dp[drinkCount][2]);
-
+        int answer = Math.max(Math.max(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
         bw.write(String.valueOf(answer));
         bw.newLine();
         bw.flush();
         bw.close();
-        br.close();
     }
 }
